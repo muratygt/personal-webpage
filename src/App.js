67ad6e66cfcc2cc399c20fe6a9c304ada032052c
Matt <muratygt@icloud.com>
2020-05-05 05:48:00 -0400

    Sidebar added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, {useState} from 'react';
-import landing from './ayse-saki.png';
+import landing from './landing.png';
+
+import {Sidebar} from 'primereact/sidebar';
+import {Button} from 'primereact/button';
+
 import './App.css';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 function App() {
-  const [landingStatus, setLandingStatus] = useState(false)
+  const [visible, setVisible] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-      {!landingStatus && <h1>Welcome to the Gender Reveal Party</h1>}
-        {!landingStatus && <button className="reveal-button" onClick={()=> setLandingStatus(true)}>Reveal</button>}
-        {landingStatus && <img src={landing} className="App-logo" alt="logo" />}
-      </header>
+      <Sidebar visible={visible} position="right" onHide={(e) => setVisible(false)}>
+        Murat Yigit
+        <ul className="list">
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+
+        </ul>
+      </Sidebar>
+      <div className="button">
+        <Button  icon="pi pi-align-justify" onClick={(e) => setVisible(true)}/>
+      </div>
+      <img src={landing} className="App-logo" alt="logo" />
+      <a href={"mailto:" + 'muratyigit88@gmail.com'}>muratyigit88@gmail.com</a>
     </div>
   );
 }
